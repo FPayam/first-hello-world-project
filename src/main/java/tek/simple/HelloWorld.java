7dab2d1248658e8b9a5a2979c18f223c80b6065d
Fawad Payam <fawadpayam@gmail.com>
2022-07-25 18:48:58 -0700

    I brought some changes please read and approve

--- src/main/java/tek/simple/HelloWorld.java
+++ src/main/java/tek/simple/HelloWorld.java
@@ -4,6 +4,6 @@ package tek.simple;
 public class HelloWorld{
 	
 	public static void main (String[]args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World project one");
 	}
 }
